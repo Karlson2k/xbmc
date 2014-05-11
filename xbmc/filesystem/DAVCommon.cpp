92f0769ec3fec4855df5d4ebd83cb3c144c07ef9
Jonathan Marshall <jmarshall@xbmc.org>
2014-05-11 21:11:15 +1200

    SQUASH: into DavCommon change

--- xbmc/filesystem/DAVCommon.cpp
+++ xbmc/filesystem/DAVCommon.cpp
@@ -45,7 +45,7 @@ bool CDAVCommon::ValueWithoutNamespace(const TiXmlNode *pNode, const std::string
     return false;
   }
 
-  std::vector<std::string> tag = StringUtils::Split(pElement->Value(), ":", 2);
+  std::vector<std::string> tag = StringUtils::Split(pElement->ValueStr(), ":", 2);
 
   if (tag.size() == 1 && tag[0] == value)
   {
