f811d46ede27e26f0713a2e870a0bd7076d9323e
Jonathan Marshall <jmarshall@xbmc.org>
2014-05-11 21:11:02 +1200

    SQUASH: into guilib sanitation

--- xbmc/guilib/VisibleEffect.cpp
+++ xbmc/guilib/VisibleEffect.cpp
@@ -188,20 +188,18 @@ CSlideEffect::CSlideEffect(const TiXmlElement *node) : CAnimEffect(node, EFFECT_
   {
     vector<string> commaSeparated = StringUtils::Split(startPos, ",");
     if (commaSeparated.size() > 1)
-    {
-      m_startX = (float)atof(commaSeparated[0].c_str());
       m_startY = (float)atof(commaSeparated[1].c_str());
-    }
+    if (!commaSeparated.empty())
+      m_startX = (float)atof(commaSeparated[0].c_str());
   }
   const char *endPos = node->Attribute("end");
   if (endPos)
   {
     vector<string> commaSeparated = StringUtils::Split(endPos, ",");
     if (commaSeparated.size() > 1)
-    {
-      m_endX = (float)atof(commaSeparated[0].c_str());
       m_endY = (float)atof(commaSeparated[1].c_str());
-    }
+    if (!commaSeparated.empty())
+      m_endX = (float)atof(commaSeparated[0].c_str());
   }
 }
 
@@ -230,10 +228,9 @@ CRotateEffect::CRotateEffect(const TiXmlElement *node, EFFECT_TYPE effect) : CAn
     {
       vector<string> commaSeparated = StringUtils::Split(centerPos, ",");
       if (commaSeparated.size() > 1)
-      {
-        m_center.x = (float)atof(commaSeparated[0].c_str());
         m_center.y = (float)atof(commaSeparated[1].c_str());
-      }
+      if (!commaSeparated.empty())
+        m_center.x = (float)atof(commaSeparated[0].c_str());
     }
   }
 }
@@ -325,10 +322,9 @@ CZoomEffect::CZoomEffect(const TiXmlElement *node, const CRect &rect) : CAnimEff
     {
       vector<string> commaSeparated = StringUtils::Split(centerPos, ",");
       if (commaSeparated.size() > 1)
-      {
-        m_center.x = (float)atof(commaSeparated[0].c_str());
         m_center.y = (float)atof(commaSeparated[1].c_str());
-      }
+      if (!commaSeparated.empty())
+        m_center.x = (float)atof(commaSeparated[0].c_str());
     }
   }
   else
