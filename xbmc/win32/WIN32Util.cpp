7cfab58f0912053509a06676478e8e74e027b6c1
Karlson2k <k2k@narod.ru>
2015-01-13 13:37:10 +0300

    WIN32Util: fopen_utf8(): fix "mode" conversion, remove CStdString, check conversion result

--- xbmc/win32/WIN32Util.cpp
+++ xbmc/win32/WIN32Util.cpp
@@ -972,9 +972,11 @@ extern "C"
 {
   FILE *fopen_utf8(const char *_Filename, const char *_Mode)
   {
-    std::string modetmp = _Mode;
-    std::wstring wfilename, wmode(modetmp.begin(), modetmp.end());
-    g_charsetConverter.utf8ToW(_Filename, wfilename, false);
+    std::wstring wfilename, wmode;
+    if (!g_charsetConverter.utf8ToW(_Filename, wfilename, false, false, true) ||
+        !g_charsetConverter.utf8ToW(_Mode, wmode, false, false, true))
+      return NULL;
+
     return _wfopen(wfilename.c_str(), wmode.c_str());
   }
 }
